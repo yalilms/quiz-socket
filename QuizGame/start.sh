b7d827ea6d0c4471319c51504de7a40348e6e02b
yalilms <yalil.ms72@gmail.com>
2026-02-19 09:36:43 +0100

    Ahora si que si terminado

--- QuizGame/start.sh
+++ QuizGame/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-java -cp app.jar org.hlanz.quiz.servidor.ServidorQuiz &
-java -cp app.jar org.hlanz.quiz.ssl.ServidorQuizSSL &
+while true; do
+  java -cp app.jar org.hlanz.quiz.servidor.ServidorQuiz
+  echo "[i] ServidorQuiz terminó, reiniciando en 3s..."
+  sleep 3
+done &
+
+while true; do
+  java -cp app.jar org.hlanz.quiz.ssl.ServidorQuizSSL
+  echo "[i] ServidorQuizSSL terminó, reiniciando en 3s..."
+  sleep 3
+done &
+
 wait
